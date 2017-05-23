a5209cd1f292e9293aec426b196fe6e430ddad25
Sam Reigle <sreigle@ackmanndickenson.com>
2017-05-23 14:59:22 -0500

    loading indicator and scroll body only

--- src/app/common/rl-scrolling-table/scrolling-table.directive.js
+++ src/app/common/rl-scrolling-table/scrolling-table.directive.js
@@ -12,21 +12,22 @@ export default function rlScrollingTable() {
   };
 
   function link(scope, element) {
-    this.heightPercentage = 0.7;
     this.container = element[0].querySelector('.rl-scrolling-table');
-    this.master = null;
+    this.heightPercentage = 0.6;
+
     this.header = null;
+    this.master = null;
     this.scrollLeft = null;
 
 
     this.init = () => {
+      scope.loading = true;
       // Clone table header
-      this.master = element[0].querySelector('.rl-table');
-      this.master.className += ' rl-table-master';
+      this.master = element[0].querySelector('.rl-table-master');
       this.header = this.master.cloneNode(true);
-      this.header.className = 'rl-table rl-table-header';
+      this.header.className = 'rl-table-header';
       this.header.querySelector('tbody').remove();
-      this.master.after(this.header);
+      this.master.before(this.header);
 
       // Set event listeners
       this.master.addEventListener('scroll', this.scroll);
@@ -36,11 +37,14 @@ export default function rlScrollingTable() {
     };
 
     this.setup = () => {
-      let $link = this;
-      setTimeout(function() {
-        $link.setHeight();
-        $link.sizeClone();
-      }, 250);
+      let masterRows = this.master.querySelectorAll('tbody tr');
+      if (masterRows.length > 0) {
+        this.setHeight();
+        this.sizeElements();
+      }
+      else {
+        setTimeout(this.setup, 250);
+      }
     };
 
     this.setHeight = () => {
@@ -50,35 +54,47 @@ export default function rlScrollingTable() {
     this.scroll = () => {
       if (!this.scrollLeft || this.scrollLeft !== this.master.scrollLeft) {
         this.scrollLeft = this.master.scrollLeft;
-        this.header.style.left = `${this.scrollLeft * -1}px`;
+        this.header.scrollLeft = this.scrollLeft;
       }
     };
 
-    this.sizeClone = () => {
+    this.sizeElements = () => {
       // Get columns from both headers
       let masterCells = this.master.querySelectorAll('tr:first-child th');
+      let masterColumns = this.master.querySelectorAll('col');
       let headerColumns = this.header.querySelectorAll('col');
 
       if (headerColumns.length === 0) {
         throw new Error('RL-SCROLLING-TABLE REQUIRES <COLGROUP> TO BE DEFINED');
       }
 
-      // Set widths for
+      // Set column widths
       let totalWidth = 0;
       angular.forEach(masterCells, (cell, index) => {
         let width = window.getComputedStyle(cell, null).getPropertyValue('width');
+        masterColumns[index].style.width = width;
         headerColumns[index].style.width = width;
         totalWidth += parseInt(width);
       });
 
-      // Set clones parent div dimensions
-      this.header.querySelector('.rl-table-clip').style.width = totalWidth + 'px';
+      // Set table widths
+      this.master.querySelector('table').style.width = `${totalWidth}px`;
+      this.header.querySelector('table').style.width = `${totalWidth}px`;
+
+      // Remove master table header
+      this.master.querySelector('thead').remove();
+
+      // Set top margin for master table
+      let headerHeight = this.header.querySelector('thead').clientHeight;
+      this.master.style.marginTop = `${headerHeight - 1}px`;
 
       // Offset .rl-table-header from .rl-table-master scrollbar
-      let scrollbarWidth = this.master.offsetWidth - this.master.clientWidth;
+      let scrollbarWidth = this.master.offsetWidth - this.master.clientWidth - 1; // Account for border-width
       if (scrollbarWidth !== 0) {
+        this.header.className += ' rl-table-header--scrollbar';
         this.header.style.right = `${scrollbarWidth}px`;
       }
+      scope.loading = false;
     };
 
     this.init();

d946ff94a7b934e2a0f6e48c3fca3467ed03dbd0
Sam Reigle <sreigle@ackmanndickenson.com>
2017-05-24 17:02:42 -0500

    static column

--- src/app/common/rl-scrolling-table/scrolling-table.directive.js
+++ src/app/common/rl-scrolling-table/scrolling-table.directive.js
@@ -12,36 +12,33 @@ export default function rlScrollingTable() {
   };
 
   function link(scope, element) {
+    this.borderWidth = 1;
     this.container = element[0].querySelector('.rl-scrolling-table');
     this.heightPercentage = 0.6;
-    this.header = null;
-    this.master = null;
-
+    this.header = element[0].querySelector('.rl-table-header');
+    this.master = element[0].querySelector('.rl-table-master');
+    this.staticColumn = element[0].querySelector('.rl-static-column');
+    this.staticHead = element[0].querySelector('.rl-static-head');
+    this.staticBody = element[0].querySelector('.rl-static-body');
 
     this.init = () => {
       scope.loading = true;
-      // Clone table header
-      this.master = element[0].querySelector('.rl-table-master');
-      this.header = this.master.cloneNode(true);
-      this.header.className = 'rl-table-header';
-      this.header.querySelector('tbody').remove();
-      this.master.before(this.header);
 
       // Set event listeners
       this.master.addEventListener('scroll', this.scroll);
       window.addEventListener('resize', this.setHeight);
 
-      this.setup();
+      this.checker();
     };
 
-    this.setup = () => {
+    this.checker = () => {
       let masterRows = this.master.querySelectorAll('tbody tr');
       if (masterRows.length > 0) {
         this.setHeight();
-        this.sizeElements();
+        this.build();
       }
       else {
-        setTimeout(this.setup, 250);
+        setTimeout(this.checker, 250);
       }
     };
 
@@ -51,44 +48,95 @@ export default function rlScrollingTable() {
 
     this.scroll = () => {
       this.header.scrollLeft = this.master.scrollLeft;
+      this.staticBody.scrollTop = this.master.scrollTop;
     };
 
-    this.sizeElements = () => {
-      // Get columns from both headers
+    this.build = () => {
+      let masterTable = this.master.querySelector('table');
       let masterCells = this.master.querySelectorAll('tr:first-child th');
       let masterColumns = this.master.querySelectorAll('col');
-      let headerColumns = this.header.querySelectorAll('col');
 
-      if (headerColumns.length === 0) {
+      if (masterColumns.length === 0) {
         throw new Error('RL-SCROLLING-TABLE REQUIRES <COLGROUP> TO BE DEFINED');
       }
 
-      // Set column widths
+      // Set widths
+      let staticColumnWidth = 0;
       let totalWidth = 0;
       angular.forEach(masterCells, (cell, index) => {
         let width = window.getComputedStyle(cell, null).getPropertyValue('width');
         masterColumns[index].style.width = width;
-        headerColumns[index].style.width = width;
         totalWidth += parseInt(width);
+        if (index === 0) {
+          staticColumnWidth = width;
+        }
       });
+      masterTable.style.width = `${totalWidth}px`;
 
-      // Set table widths
-      this.master.querySelector('table').style.width = `${totalWidth}px`;
-      this.header.querySelector('table').style.width = `${totalWidth}px`;
+      // Clone table header
+      this.header.appendChild(masterTable.cloneNode(true));
+      this.header.querySelector('tbody').remove();
 
-      // Remove master table header
-      this.master.querySelector('thead').remove();
-
-      // Set top margin for master table
+      // Table offsets
       let headerHeight = this.header.querySelector('thead').clientHeight;
-      this.master.style.marginTop = `${headerHeight - 1}px`;
+      this.master.style.marginTop = `${headerHeight - this.borderWidth}px`;
+      this.master.style.marginLeft = `${staticColumnWidth}`;
+      this.header.style.left = `${staticColumnWidth}`;
 
-      // Offset .rl-table-header from .rl-table-master scrollbar
-      let scrollbarWidth = this.master.offsetWidth - this.master.clientWidth - 1; // Account for border-width
+      // Scrollbar offsets
+      let scrollbarWidth = this.master.offsetWidth - this.master.clientWidth - this.borderWidth;
+      let scrollbarHeight = this.master.offsetHeight - this.master.clientHeight - this.borderWidth;
       if (scrollbarWidth !== 0) {
         this.header.className += ' rl-table-header--scrollbar';
-        this.header.style.right = `${scrollbarWidth}px`;
+        this.header.style.right = `${scrollbarWidth - this.borderWidth}px`;
+        this.staticColumn.style.width = `${staticColumnWidth}`;
+        this.staticColumn.style.bottom = `${scrollbarHeight}px`;
       }
+
+      // Static column table
+      let staticTable = masterTable.cloneNode(true);
+      staticTable.style.width = '';
+
+      // Remove non-static columns from static column table
+      let staticRows = staticTable.querySelectorAll('tr');
+      angular.forEach(staticRows, (row) => {
+        let rowCells = row.querySelectorAll('th, td');
+        angular.forEach(rowCells, (cell, index) => {
+          if (index !== 0) {
+            cell.remove();
+          }
+        });
+      });
+
+      // Create static head table
+      this.staticHead.appendChild(staticTable.cloneNode(true));
+      this.staticHead.querySelector('tbody').remove();
+
+      // Create static body table
+      this.staticBody.appendChild(staticTable.cloneNode(true));
+      this.staticBody.querySelector('thead').remove();
+      this.staticBody.style.top = window.getComputedStyle(this.staticHead, null).getPropertyValue('height');
+
+      // Remove master thead
+      this.master.querySelector('thead').remove();
+
+      // Remove static column from other tables
+      let rowSelectors = [
+        '.rl-table-header tr',
+        '.rl-table-header colgroup',
+        '.rl-table-master tr',
+        '.rl-table-master colgroup'
+      ];
+      let otherRows = this.container.querySelectorAll(rowSelectors.join());
+      angular.forEach(otherRows, (row) => {
+        let rowCells = row.querySelectorAll('col, th, td');
+        angular.forEach(rowCells, (cell, index) => {
+          if (index === 0) {
+            cell.remove();
+          }
+        });
+      });
+
       scope.loading = false;
     };
 

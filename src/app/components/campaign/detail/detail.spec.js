e99c1cb8aa1cfcdb1a2c86039b426ded8a85642c
Sam Reigle <sreigle@ackmanndickenson.com>
2017-07-11 12:09:06 -0500

    Update campaign details unit tests

--- src/app/components/campaign/detail/detail.spec.js
+++ src/app/components/campaign/detail/detail.spec.js
@@ -5,11 +5,26 @@ import mockRlConfig from '../mock-data/rlConfig.json';
 
 describe('components.campaign.detail', () => {
 
-  let $ctrl, service, dataSettings;
+  let $ctrl, $scope, service;
+
+  let dataService = {
+    selectedSettings: {
+      breakdownType: 'cycles'
+    },
+    getSelectedBreakdownType: angular.noop,
+    getSelectedRangeParams: angular.noop
+  };
+
+  let mockChange = {
+    campaignOverview: {
+      currentValue: mockCampaignOverview
+    }
+  };
 
   beforeEach(() => {
     angular.mock.module('campaign.detail', ($provide) => {
       $provide.value('CampaignSidebar', {});
+      $provide.value('DataSettingsService', dataService);
       $provide.value('rlConfig', mockRlConfig);
     });
 
@@ -20,37 +35,48 @@ describe('components.campaign.detail', () => {
 
     angular.mock.inject(($injector) => {
       let $componentController = $injector.get('$componentController');
-      let stateParams = {
-        mcid: 123456
-      };
+      $scope = $injector.get('$rootScope').$new();
       service = $injector.get('CampaignDetailService');
-      dataSettings = $injector.get('DataSettingsService');
-
       $ctrl = $componentController('campaign.detail', {
-        $stateParams: stateParams
+        $scope: $scope
       }, bindings);
     });
+    spyOn(service, 'getPerformanceData').and.callThrough();
+    spyOn(service, 'getAgeGenderData').and.callThrough();
+    spyOn(service, 'getDeviceData').and.callThrough();
   });
 
   it('constructs', () => {
     expect($ctrl.ageGenderData).toBeNull();
+    expect($ctrl.deviceData).toBeNull();
     expect($ctrl.performanceData).toBeNull();
+    expect($ctrl.dataSettingsService).toEqual(dataService);
     expect($ctrl.service).toEqual(service);
   });
 
   it('$onChanges', () => {
     let breakdown = 'cycles';
-    let params = {start: '2017/3', end: '2017/5'};
-    spyOn(service, 'getPerformanceData').and.callThrough();
-    spyOn(dataSettings, 'getSelectedBreakdownType').and.callFake(() => breakdown);
-    spyOn(dataSettings, 'getSelectedRangeParams').and.callFake(() => params);
-    $ctrl.$onChanges({
-      campaignOverview: {
-        currentValue: mockCampaignOverview
-      }
-    });
+    let params = {start: 1, end: 9};
+    spyOn(dataService, 'getSelectedBreakdownType').and.callFake(() => breakdown);
+    spyOn(dataService, 'getSelectedRangeParams').and.callFake(() => params);
+    $ctrl.$onChanges(mockChange);
     expect($ctrl.campaign).toEqual(mockCampaignOverview);
     expect(service.getPerformanceData).toHaveBeenCalledWith(mockCampaignOverview.masterCampaignId, breakdown, params);
+    expect(service.getPerformanceData).toHaveBeenCalledWith(mockCampaignOverview.masterCampaignId, breakdown, params);
+    expect(service.getPerformanceData).toHaveBeenCalledWith(mockCampaignOverview.masterCampaignId, breakdown, params);
+  });
+
+  it('$watch', () => {
+    let breakdown = 'days';
+    let params = {start: '2017-03-01', end: '2017-05-01'};
+    spyOn(dataService, 'getSelectedBreakdownType').and.callFake(() => breakdown);
+    spyOn(dataService, 'getSelectedRangeParams').and.callFake(() => params);
+    $ctrl.campaign = mockCampaignOverview;
+    dataService.selectedSettings.breakdownType = 'days';
+    $scope.$digest();
+    expect(service.getPerformanceData).toHaveBeenCalledWith(mockCampaignOverview.masterCampaignId, breakdown, params);
+    expect(service.getAgeGenderData).toHaveBeenCalledWith(mockCampaignOverview.masterCampaignId, breakdown, params);
+    expect(service.getDeviceData).toHaveBeenCalledWith(mockCampaignOverview.masterCampaignId, breakdown, params);
   });
 
 });

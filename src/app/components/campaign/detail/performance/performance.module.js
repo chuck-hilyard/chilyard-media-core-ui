e6211224dfaa394c4a00ba0bad799bc130faf310
Sam Reigle <sreigle@ackmanndickenson.com>
2017-07-12 10:32:37 -0500

    performance table unit tests

--- src/app/components/campaign/detail/performance/performance.module.js
+++ src/app/components/campaign/detail/performance/performance.module.js
@@ -1,9 +1,11 @@
+import ngTranslate from 'angular-translate';
 import tooltip from 'angular-ui-bootstrap/src/tooltip';
 import component from './performance.component';
 import './performance.scss';
 
 export default angular
   .module('campaign.detail.performance', [
+    ngTranslate,
     tooltip
   ])
   .config(($uibTooltipProvider) => {

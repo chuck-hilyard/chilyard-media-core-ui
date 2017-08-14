784ae63ceb6eee8514072213c091ad1b8d74f2c8
Sam Reigle <sreigle@ackmanndickenson.com>
2017-08-14 15:18:44 -0500

    Contextual components for campaign overview

--- test/mocks/common/common.mocks.js
+++ test/mocks/common/common.mocks.js
@@ -1,9 +1,11 @@
 import api from './rl-api/mock-api.js';
 import config from './rl-config/mock-config.js';
 import logger from './rl-logger/mock-logger.js';
+import StubChanges from 'angular-stub-changes';
 
 export default {
   api: api,
   config: config,
-  logger: logger
+  logger: logger,
+  StubChanges: StubChanges
 };

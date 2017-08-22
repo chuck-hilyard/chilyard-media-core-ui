72cb9859c91fb62b752faf90ea1177285245db56
Martin Ivanov <30518671+martin-iv-accedia@users.noreply.github.com>
2017-08-22 10:26:27 +0300

    Fix broken tests (#112)

--- src/app/components/home/search-dashboard/search-dashboard.spec.js
+++ src/app/components/home/search-dashboard/search-dashboard.spec.js
@@ -1,9 +1,14 @@
+import commonMocks from '../../../../../test/mocks/common/common.mocks';
+
 describe('components.home.search-dashboard', () => {
 
   let $ctrl;
 
   beforeEach(() => {
     angular.mock.module('searchDashboard');
+    angular.mock.module(($provide) => {
+      $provide.value('rlApi', commonMocks.rlApi);
+    })
 
     angular.mock.inject(($injector) => {
       let $componentController = $injector.get('$componentController');
